7b29ce8bc6883367af200fe4ec6aea29f681ccfc
zineb essoussi <zineb.zineb.zouzou@gmail.com>
2023-04-20 23:56:46 +0000

    home page completed

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+$('.owl-carousel').owlCarousel({
+loop:true,
+margin:10,
+responsiveClass:true,
+responsive:{
+    0:{
+        items:1,
+        nav:true
+    },
+    400:{
+        items:2,
+        nav:false
+    },
+    500:{
+        items:3,
+        nav:false
+    },
+    1000:{
+        items:5,
+        nav:true,
+        loop:false
+    }
+}
+})
